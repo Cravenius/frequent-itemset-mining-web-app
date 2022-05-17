4b279a49385a821ffa453baffef74f680541e135
Yunata Gunawan <89584416+Cravenius@users.noreply.github.com>
2022-05-17 17:09:12 +0700

    more dynamic plot

--- app.py
+++ app.py
@@ -5,19 +5,15 @@
 import pandas as pd
 import os
 from flask import Flask, request, jsonify, render_template, flash, request, redirect, url_for, send_from_directory
-from sklearn import metrics
 from werkzeug.utils import secure_filename
 from mlxtend.preprocessing import TransactionEncoder
-from mlxtend.frequent_patterns import apriori, association_rules, fpgrowth, fpmax
-import seaborn as sns
+from mlxtend.frequent_patterns import association_rules, fpgrowth
 import base64
-import matplotlib.pyplot as plt
 from io import BytesIO
-from matplotlib.figure import Figure
-from pandas.plotting import parallel_coordinates
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import plotly
+import plotly.express as px
+import json
 
-plt.switch_backend('agg')
 ALLOWED_EXTENSIONS = ['.xlsx', '.xlx', '.csv', '.xml']
 app = Flask(__name__)
 
@@ -38,11 +34,31 @@ def preproces_dataset(path_filename):
   items_df = pd.DataFrame(te_ary, columns=te.columns_)
   return items_df
 
-def rules_to_coordinates(rules):
-  rules['antecedent'] = rules['antecedents'].apply(lambda antecedent: list(antecedent)[0])
-  rules['consequent'] = rules['consequents'].apply(lambda consequent: list(consequent)[0])
-  rules['rule'] = rules.index
-  return rules[['antecedent','consequent','rule']]
+def plotly_heatmap(rules, filter, color, title):
+  st = rules.pivot(index='consequents', columns='antecedents', values=filter)
+  st = st.fillna(0)
+  for col in st.columns:
+    st[col] = pd.to_numeric(st[col],errors = 'coerce')
+  print(st.info())
+  fig = px.imshow(st, labels=dict(x="Item", y="Item", color="Filter: {}".format(filter), title=title),
+    x=st.columns.tolist(),
+    y=st.index.tolist(),
+    aspect="auto", text_auto=True,
+    color_continuous_scale=color,
+    )
+  fig.update_xaxes(side="top")
+  fig.update_layout(width=1250, height=600, margin=dict(l=20, r=20, t=20, b=20))
+  fig.layout.autosize = True
+  plot_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+  return plot_json
+
+def plotly_scatter(df, filter, title):
+  fig = px.scatter(df, x="support", y="confidence", color=filter, marginal_y="violin",
+           marginal_x="box", trendline="ols", template="simple_white", title=title)
+  fig.update_xaxes(side="top")
+  fig.update_layout(width=1250, height=800)
+  plot_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+  return plot_json
 
 @app.route('/')
 def home():
@@ -88,11 +104,18 @@ def upload_file():
     fpgrowthcolnames = None
     rulesrecords = None
     rulescolnames = None
-    heatmap_plot = []
+    scatter_support = None
+    scatter_filter = None
+    heatmap_support = None
+    heatmap_filter = None
 
     # assign data
     if '1' in checkbox:
       fpgrowthdf = fpgrowth(items_df, min_support=minSup, use_colnames=True, max_len=maxItemsets)
+      fpgrowthdf['length'] = fpgrowthdf['itemsets'].apply(lambda x: len(x))
+      fpgrowthrecords = fpgrowthdf.to_dict('records')
+      fpgrowthcolnames = fpgrowthdf.columns.values
+      
       rules = association_rules(fpgrowthdf, min_threshold=minThres, metric=filter)
       rules.drop(rules.columns[[2, 3]], axis = 1, inplace = True)
       rules["support"] = rules["support"].apply(lambda x: format(float(x),".5f"))
@@ -103,78 +126,24 @@ def upload_file():
       rulesrecords = rules.to_dict('records')
       rulescolnames = rules.columns.values
 
-      fpgrowthdf['length'] = fpgrowthdf['itemsets'].apply(lambda x: len(x))
-      fpgrowthrecords = fpgrowthdf.to_dict('records')
-      fpgrowthcolnames = fpgrowthdf.columns.values
-
-      # Scatter Plot
-      fig, ax = plt.subplots( nrows=1, ncols=1 )  # create figure & 1 axis
-      sns.scatterplot(x = "support", y = "confidence", data = rules)
-      plt.margins(0.01, 0.01)
-      plt.xticks(rotation='vertical')
-      fig.savefig('static/scatter_plot.png')   # save the figure to file
-      plt.close(fig)
-
-      fig, ax = plt.subplots( nrows=1, ncols=1 )  # create figure & 1 axis
-      sns.scatterplot(x = "support", y = "confidence", size = filter, data = rules)
-      plt.margins(0.01, 0.01)
-      plt.xticks(rotation='vertical')
-      fig.savefig('static/scatter_plot_optimal.png')   # save the figure to file
-      plt.close(fig)
-
-      # Parallel Coordinates Plot
-      fig, ax = plt.subplots( nrows=1, ncols=1 )  # create figure & 1 axis
-      coords = rules_to_coordinates(rules)
-      parallel_coordinates(coords, 'rule')
-      plt.legend([])
-      plt.grid(True)
-      fig.savefig('static/parallel_coordinates.png')   # save the figure to file
-      plt.close(fig)
+      scatter_support = plotly_scatter(rules, 'support', 'scatter plot with support filter')
+      scatter_filter = plotly_scatter(rules, filter, 'scatter plot with {} filter'.format(filter))
 
       # Convert antecedents and consequents into strings
       rules['antecedents'] = rules['antecedents'].apply(lambda a: ','.join(list(a)))
       rules['consequents'] = rules['consequents'].apply(lambda a: ','.join(list(a)))
-
-      fig, ax = plt.subplots( nrows=1, ncols=1 )  # create figure & 1 axis
-      support_table = rules.pivot(index='consequents', columns='antecedents', values='support')
-      support_table = support_table.fillna(0)
-      for col in support_table.columns:
-        support_table[col] = pd.to_numeric(support_table[col],errors = 'coerce')
-      print(support_table.info())
-
-      sns.heatmap(support_table, annot=True, cbar=False, ax=ax)
-      b, t = plt.ylim() 
-      b += 0.5 
-      t -= 0.5 
-      plt.ylim(b, t) 
-      plt.yticks(rotation=0)
-      plt.margins(0.01, 0.01)
-      fig.savefig('static/heatmap.png')   # save the figure to file
-      plt.close(fig)
-
-      fig, ax = plt.subplots( nrows=1, ncols=1 )  # create figure & 1 axis
-      support_table = rules.pivot(index='consequents', columns='antecedents', values=filter)
-      support_table = support_table.fillna(0)
-      for col in support_table.columns:
-        support_table[col] = pd.to_numeric(support_table[col],errors = 'coerce')
-      print(support_table.info())
-      
-      sns.heatmap(support_table, annot=True, cbar=False, ax=ax)
-      b, t = plt.ylim() 
-      b += 0.5 
-      t -= 0.5 
-      plt.ylim(b, t) 
-      plt.yticks(rotation=0)
-      plt.margins(0.01, 0.01)
-      fig.savefig('static/heatmap_optimal.png')   # save the figure to file
-      plt.close(fig)
+   
+      heatmap_support = plotly_heatmap(rules, 'support', 'RdPu', 'Support Correlation Table')
+      heatmap_filter = plotly_heatmap(rules, filter, 'GnBu', "{} Correlation Table".format(filter))
 
     del items_df
     del rules
     del fpgrowthdf
     return render_template('MiningAnalysis.html', algorithms=checkbox,
       fpgrowthrecords=fpgrowthrecords, fpgrowthcolnames=fpgrowthcolnames,
-      rulesrecords=rulesrecords, rulescolnames=rulescolnames
+      rulesrecords=rulesrecords, rulescolnames=rulescolnames,
+      heatmap_support=heatmap_support, heatmap_filter=heatmap_filter, 
+      scatter_support=scatter_support, scatter_filter=scatter_filter
       )
 
 if __name__ == '__main__':
